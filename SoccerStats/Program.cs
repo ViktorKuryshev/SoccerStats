6fd9eb894bf71ee6132d65289bd782de22440ef9
Victor Kuryshev <sales@gtmk.ru>
2017-12-24 09:35:15 +0300

    Использовали микросовтавские апи для поиска(новостей не удалось, но страницы ищет)

--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Net;
 using Newtonsoft.Json;
 
 namespace SoccerStats
@@ -12,19 +13,26 @@ namespace SoccerStats
     {
         static void Main(string[] args)
         {
-            string currentDirectory = Directory.GetCurrentDirectory();
-            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
-            var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
-            var fileContents = ReadSoccerResults(fileName);
+			string currentDirectory = Directory.GetCurrentDirectory();
+			DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+			var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
+			var fileContents = ReadSoccerResults(fileName);
 			fileName = Path.Combine(directory.FullName, "players.json");
 			var players = DeserializePlayers(fileName);
-	        var topTenPlayers = GetTopTenPlayers(players);
+			var topTenPlayers = GetTopTenPlayers(players);
 
 			foreach (var player in topTenPlayers)
 			{
-				Console.WriteLine("Name: " + player.FirstName + " PPG: " + player.PointsPerGame);
+				List<NewsResult> newsResults = GetNewsForPlayer(String.Format("{0} {1}", player.FirstName, player.SecondName));
+				foreach (var result in newsResults)
+				{
+					Console.WriteLine(String.Format("Name: {0} Url: {1} \r\n", result.name, result.url));
+					Console.ReadKey();
+				}
 			}
-
+			fileName = Path.Combine(directory.FullName, "topten.json");
+			SerializePlayersToFile(topTenPlayers, fileName);
+			
 		}
 
         public static string ReadFile(string fileName)
@@ -119,5 +127,46 @@ namespace SoccerStats
 		    }
 		    return topTenPlayers;
 	    }
-    }
+
+	    public static void SerializePlayersToFile(List<Player> players, string fileName)
+	    {
+			var serializer = new JsonSerializer();
+		    using (var writer = new StreamWriter(fileName))
+		    using (var jasonWriter = new JsonTextWriter(writer))
+		    {
+			    serializer.Serialize(jasonWriter, players);
+		    }
+		   
+		}
+
+	    public static string GetHomeGooglePage()
+	    {
+		    var webClient = new WebClient();
+		    byte[] googleHome = webClient.DownloadData("https://www.google.com");
+
+			using (var stream = new MemoryStream(googleHome))
+			using(var reader = new StreamReader(stream))
+			{
+				return reader.ReadToEnd();
+			}
+	    }
+
+	    public static List<NewsResult> GetNewsForPlayer(string playerName)
+	    {
+		    var results = new List<NewsResult>();
+			var webClient = new WebClient();
+			webClient.Headers.Add("Ocp-Apim-Subscription-Key", "43b127415c9242baa9325d79bdd65267");
+		    byte[] searchResults = webClient.DownloadData(string.Format("https://api.cognitive.microsoft.com/bing/v7.0/search?q={0}&mkt=en-us", Uri.EscapeDataString(playerName)));
+			var serializer = new JsonSerializer();
+
+			using (var stream = new MemoryStream(searchResults))
+			using (var reader = new StreamReader(stream))
+				using(var jsonReader = new JsonTextReader(reader))
+				{
+					results = serializer.Deserialize<NewsSearch>(jsonReader).webPages.NewsResults;
+				}
+
+		    return results;
+	    }
+	}
 }
