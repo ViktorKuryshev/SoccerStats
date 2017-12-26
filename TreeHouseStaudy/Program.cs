5ff2a70321363257aa9ff0058115277ccf3b7455
Viktor Kuryshev <sales@gtmk.ru>
2017-12-26 22:03:03 +0300

    Коллекции, написал свой метод сравнения для класса

---
+++ TreeHouseStaudy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeHouseStaudy
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Student> students = new List<Student>()
+			{
+				new Student() {Name = "Sali", GradeLevel = 3 },
+				new Student() {Name = "Bob", GradeLevel = 2 },
+				new Student() {Name = "Sali", GradeLevel = 2 },
+				new Student() {Name = "Mat", GradeLevel = 4 },
+				
+			};
+
+			students.Sort();
+
+			foreach(Student student in students)
+			{
+				Console.WriteLine($"{student.Name} is in grade {student.GradeLevel}");
+			}
+		}
+	}
+}
